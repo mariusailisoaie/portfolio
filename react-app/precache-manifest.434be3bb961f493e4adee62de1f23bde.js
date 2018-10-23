05127dd95d94a775d88b3aa3e0f538786a204014
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-23 21:16:24 +0200

    Add: create react app

---
+++ react-app/precache-manifest.434be3bb961f493e4adee62de1f23bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc85c12c2470705dfbfb",
+    "url": "/static/js/main.fc85c12c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "fc85c12c2470705dfbfb",
+    "url": "/static/css/main.0f75fe6e.chunk.css"
+  },
+  {
+    "revision": "704f6e0edd7956f65ad40f3e5d06a4ba",
+    "url": "/index.html"
+  }
+];
