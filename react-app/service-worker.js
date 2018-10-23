c37c1acb76aa255aab9857c571d05bb017d15057
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-23 21:48:39 +0200

    Updated package.json and rebuilt app

--- react-app/service-worker.js
+++ react-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.6d8dc3391133f4a7bc7228e68e5d17a5.js"
+  "/react-app/precache-manifest.93d74e0584ecde540c381d37c7bf5891.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/build/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
