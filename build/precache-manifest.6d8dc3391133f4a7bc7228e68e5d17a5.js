06c16a4ac774935916d2608726b64ab5b469bf2c
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-23 21:42:47 +0200

    Updated json file and rebuilt app

---
+++ build/precache-manifest.6d8dc3391133f4a7bc7228e68e5d17a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "2b62e1f32fb0a0acbf4d",
+    "url": "/build/static/js/main.2b62e1f3.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/build/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2b62e1f32fb0a0acbf4d",
+    "url": "/build/static/css/main.0f75fe6e.chunk.css"
+  },
+  {
+    "revision": "2af877cc22011e98611db4b5b8ec7c59",
+    "url": "/build/index.html"
+  }
+];
