63f96cdf93ee46281456c68d039ea87cca85995a
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-24 12:59:13 +0200

    Add: todo-app react

---
+++ todo-react/precache-manifest.854e4f16479c08d2551894dfabb89e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "a27edb595a5c5f1c228f",
+    "url": "/react-todo/static/js/main.a27edb59.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-todo/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "a27edb595a5c5f1c228f",
+    "url": "/react-todo/static/css/main.2b9a5f0c.chunk.css"
+  },
+  {
+    "revision": "d6c2c9be8230c7b4f72fa2faf3e75440",
+    "url": "/react-todo/index.html"
+  }
+];
