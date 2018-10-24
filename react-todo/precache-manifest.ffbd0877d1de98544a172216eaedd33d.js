c10962e6d1b46bb44fecbcd2f538522b4d387a2c
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-24 12:56:09 +0200

    Add: todo app made with react

---
+++ react-todo/precache-manifest.ffbd0877d1de98544a172216eaedd33d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "1c8552bc51645a930fb1",
+    "url": "/react-todo/static/js/main.1c8552bc.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-todo/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "1c8552bc51645a930fb1",
+    "url": "/react-todo/static/css/main.0f75fe6e.chunk.css"
+  },
+  {
+    "revision": "ad749844824baa098b14e919798142be",
+    "url": "/react-todo/index.html"
+  }
+];
