f2c7537b1ca1e3ea1a4548dfedf6b358a91570c3
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-10-25 19:31:50 +0200

    Add: random chuck norris joke generator

---
+++ jokes-generator/js/app.js
@@ -0,0 +1,12 @@
+const getRandomDadJoke = async () => {
+  const response = await fetch('http://api.icndb.com/jokes/random');
+  const responseData = await response.json();
+  return responseData;
+}
+
+const getJoke = () => {
+  getRandomDadJoke()
+    .then(data => document.getElementById('joke-container').textContent = data.value.joke)
+    .catch(err => console.log(err));
+}
+document.getElementById('joke-button').addEventListener('click', getJoke);
