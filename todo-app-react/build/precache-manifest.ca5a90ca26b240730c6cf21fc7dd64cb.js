16ec9dda0769088d1775898b384ae4cf24614d40
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-09 16:33:36 +0100

    Built todo app react for deployment

---
+++ todo-app-react/build/precache-manifest.ca5a90ca26b240730c6cf21fc7dd64cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "e634bb1c63799831bd0d",
+    "url": "/react-todo/static/js/main.e634bb1c.chunk.js"
+  },
+  {
+    "revision": "b36c5939986e1f29f72d",
+    "url": "/react-todo/static/js/1.b36c5939.chunk.js"
+  },
+  {
+    "revision": "e634bb1c63799831bd0d",
+    "url": "/react-todo/static/css/main.cdfac43d.chunk.css"
+  },
+  {
+    "revision": "2f6db1e834b4928134e458e80e2e064f",
+    "url": "/react-todo/index.html"
+  }
+];
