7a945985192e6c73d023a8abc05970a5e9d7bb08
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-09 16:38:46 +0100

    Updated path to todo app react

--- todo-app-react/build/service-worker.js
+++ todo-app-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-todo/precache-manifest.ca5a90ca26b240730c6cf21fc7dd64cb.js"
+  "/react-todo/build/precache-manifest.b5cd900c960bf69ee7e0bc939b057011.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-todo/index.html", {
+workbox.routing.registerNavigationRoute("/react-todo/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
