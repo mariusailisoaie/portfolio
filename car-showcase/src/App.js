163d786cca462880afb5fc7daeecd7d54962eee5
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-11 22:09:28 +0100

    Add copencars project made with react

---
+++ car-showcase/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center">Welcome to CopenCars</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
