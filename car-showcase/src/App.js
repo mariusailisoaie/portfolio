19a0c95359ec705ec814d04085ff0f2b11dea63d
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-12 01:28:18 +0100

    Add: cars array in state and pass it to listcars component

--- car-showcase/src/App.js
+++ car-showcase/src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import ListCars from './components/ListCars';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 class App extends Component {
+  state = {
+    cars: [
+      {id: 1, brand: 'VW', model: 'Golf 7', price: 'EUR 25.000'},
+      {id: 2, brand: 'Skoda', model: 'Octavia', price: 'EUR 22.000'},
+      {id: 3, brand: 'Mercedez', model: 'Benz', price: 'EUR 55.000'}
+    ]
+  }
   render() {
     return (
       <BrowserRouter>
@@ -15,6 +23,8 @@ class App extends Component {
           <Route exact path='/' component={ Home } />
           <Route path='/about' component={ About } />
         </Switch>
+
+        <ListCars cars={this.state.cars} />
 
         </div>
       </BrowserRouter>
