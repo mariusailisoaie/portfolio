0f4f91c258b2392b053b2bc7f7c616b4281d5563
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-12 01:25:57 +0100

    Fix: navbar width

--- car-showcase/src/components/Navbar.js
+++ car-showcase/src/components/Navbar.js
@@ -3,15 +3,13 @@ import { Link, withRouter } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="nav-wrapper blue lighten-3">
-        <Link to="/" className="brand-logo">CopenCars</Link>
+    <nav className="nav-wrapper blue lighten-3">
+      <Link to="/" className="brand-logo">CopenCars</Link>
 
-        <ul className="right">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </div>
+      <ul className="right">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </nav>
   )
 }
