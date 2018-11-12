b4758b54df03ca2cdf9192d15f039ca409c70e32
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-11-12 11:49:05 +0100

    Update: production build

---
+++ car-showcase/build/precache-manifest.290f37f8dba879c24661f2cfe6de4185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48433d2d5858c75824dc",
+    "url": "/car-showcase/build/static/js/runtime~main.48433d2d.js"
+  },
+  {
+    "revision": "6eb843c47d55623f094d",
+    "url": "/car-showcase/build/static/js/main.6eb843c4.chunk.js"
+  },
+  {
+    "revision": "e28a94bfcbb23da0f27e",
+    "url": "/car-showcase/build/static/js/1.e28a94bf.chunk.js"
+  },
+  {
+    "revision": "6eb843c47d55623f094d",
+    "url": "/car-showcase/build/static/css/main.7bda3255.chunk.css"
+  },
+  {
+    "revision": "2b35d34bf8a8d942df581b7c59fac403",
+    "url": "/car-showcase/build/index.html"
+  }
+];
--- car-showcase/build/service-worker.js
+++ car-showcase/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/car-showcase/build/precache-manifest.66de11bcbbd163061ce1a919251db61e.js"
+  "/car-showcase/build/precache-manifest.290f37f8dba879c24661f2cfe6de4185.js"
 );
 
 workbox.clientsClaim();
